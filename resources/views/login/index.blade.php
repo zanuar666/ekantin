6b41e28b700d2978644d99d15845be70bf2c585b
much12 <muhammadarizalfauzi2@gmail.com>
2020-09-29 23:11:45 +0700

    Adding bootstrap, fontawesome, jQuery, Popper and login design

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-5.14.0/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+    <title>Login - E-Kantin</title>
+
+    <style>
+        body {
+            background: url(<?= asset('assets/img/bg.jpg') ?>);
+            background-size: cover;
+            background-repeat: repeat-y;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="login-box">
+        <div class="card m-auto">
+            <div class="card-body">
+                <h1 class="text-center text-white">E-Kantin</h1>
+
+                <div class="form-input">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="fa fa-user"></i>
+                                </span>
+                            </div>
+
+                            <input type="text" name="username" class="form-control" placeholder="Username" required>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">
+                                    <i class="fa fa-lock"></i>
+                                </span>
+                            </div>
+
+                            <input type="text" name="password" class="form-control" placeholder="Password" required>
+                        </div>
+                    </div>
+
+                    <button type="button" class="btn btn-primary btn-flat btn-block">LOGIN</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="{{ asset('assets/js/jquery-3.2.1.slim.min.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+</body>
+
+</html>
