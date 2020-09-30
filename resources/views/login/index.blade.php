63047143fe1f6598a1e2d0e99d4a40459be8557a
much12 <muhammadarizalfauzi2@gmail.com>
2020-09-30 23:13:44 +0700

    Adding favicon skariga and changing color of button login to yellow (btn-warning)

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -3,13 +3,24 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-5.14.0/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 
     <title>Login - E-Kantin</title>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
     <style>
         body {
@@ -57,7 +68,7 @@
                                         </div>
                                     </div>
 
-                                    <button type="submit" class="btn btn-primary btn-flat btn-block">LOGIN</button>
+                                    <button type="submit" class="btn btn-warning btn-flat btn-block">LOGIN</button>
                                 </div>
                             </form>
                         </div>
