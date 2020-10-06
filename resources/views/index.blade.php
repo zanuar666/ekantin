4eb794c22886ed35b6004ed30f8d89cd1cc8b100
Muhammad Andika <dikapolk@gmail.com>
2020-10-06 23:01:42 +0700

    Updating Views

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,15 +23,36 @@
     <![endif]-->
 </head>
 
+{{-- <style>
+    .sidebarr {
+        width: 20%;
+        height: 100%;
+    }
+
+    .sidebar {
+        padding-top: 50%;
+    }
+
+    .logos {
+        height: 100px;
+        width: 100px;
+    }
+
+    .navi {
+        padding-top: 10%;
+    }
+</style> --}}
+
 <body>
     <nav class="navbar navbar-expand-sm navbar-dark bg-custom fixed-top">
         <a class="navbar-brand" href="{{ url('/') }}">E-Kantin</a>
 
         <ul class="nav ml-auto">
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-white" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                <a class="nav-link dropdown-toggle text-white" data-toggle="dropdown" href="#" role="button"
+                    aria-haspopup="true" aria-expanded="false">
                     <i class="fa fa-user"></i>
-                    Moch Arizal Fauzi
+                    {{session('name')}}
                 </a>
 
                 <div class="dropdown-menu">
@@ -42,7 +63,7 @@
 
                     <div class="dropdown-divider"></div>
 
-                    <a class="dropdown-item" href="#">
+                    <a class="dropdown-item" href="logout">
                         <i class="fa fa-sign-out-alt"></i>
                         Logout
                     </a>
@@ -50,6 +71,24 @@
             </li>
         </ul>
     </nav>
+
+    {{-- <aside class="sidebarr">
+        <div class="sidebar text-center">
+            <div class="profile-image">
+                <img src="https://bnpb.go.id/uploads/organization/default.jpg" alt="" class="img-fluid img-thumbnail rounded-circle logos">
+                <h6>{{session('name')}}</h6>
+    </div>
+    </div>
+    <nav class="mt-3 navi">
+        <ul class="nav nav-pills nav-sidebar flex-column">
+            <li class="nav-item">
+                <a href="#" class="nav-link active">
+                    <i class="nav-icon fas fa-tachometer-alt"></i> Dashboard
+                </a>
+            </li>
+        </ul>
+    </nav>
+    </aside> --}}
 
     <script src="{{ asset('assets/js/jquery-3.5.1.min.js') }}"></script>
     <script src="{{ asset('assets/js/popper.min.js') }}"></script>
